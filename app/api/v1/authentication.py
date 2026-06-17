90a52aa477943cf4b9d4ccdea0f109aff5fd5acd
personalbuse <defaultuser965@gmail.com>
2026-06-16 19:02:52 -0500

    fix: correccion not BackupCode.used a BackupCode.used == False en twofa_login_backup

--- app/api/v1/authentication.py
+++ app/api/v1/authentication.py
@@ -879,7 +879,7 @@ async def twofa_login_backup(
     stmt_bc = select(BackupCode).where(
         BackupCode.user_id == user.id,
         BackupCode.hashed_code == hashed,
-        not BackupCode.used,
+        BackupCode.used == False,
     )
     result_bc = await db.execute(stmt_bc)
     bc = result_bc.scalar_one_or_none()
